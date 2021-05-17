3721526a12df964bef94a22f72502d513e0634de
BuildTools <unconfigured@null.spigotmc.org>
2021-05-17 10:08:03 -0500

    First Commit

---
+++ src/main/java/net/thejrdev/EasyYML.java
@@ -0,0 +1,77 @@
+package net.thejrdev;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Set;
+
+public class EasyYML {
+
+    private FileConfiguration config;
+
+    public EasyYML(@NotNull FileConfiguration config) {
+        this.config = config;
+    }
+
+    public ItemStack createItemStack(@NotNull String path) throws IllegalArgumentException, NullPointerException, MissingConfigArgument {
+
+        ConfigurationSection section = config.getConfigurationSection(path);
+
+        Set<String> keys = section.getKeys(false);
+        if(!keys.contains("material")){
+            throw new MissingConfigArgument("\"material\" is missing from the configuration file");
+        }
+        Material material = Material.valueOf(section.getString("material"));
+        ItemStack item = new ItemStack(material);
+
+        ItemMeta meta = item.getItemMeta();
+
+        if(keys.contains("name")){
+            meta.setDisplayName(section.getString("name"));
+        }
+
+        if(keys.contains("lore")){
+            meta.setLore(section.getStringList("lore"));
+        }
+
+        if(keys.contains("enchantments")){
+            ConfigurationSection enchantSection = section.getConfigurationSection("enchantments");
+            Set<String> enchants = enchantSection.getKeys(false);
+            for(String s: enchants){
+                Enchantment ench = Enchantment.getByKey(NamespacedKey.minecraft(s));
+                meta.addEnchant(ench, enchantSection.getInt(s), false);
+
+
+            }
+        }
+
+        item.setItemMeta(meta);
+        return item;
+    }
+
+
+
+
+}
+
+/*
+
+item:
+    material: <material value>
+    name: <display name>
+    lore:
+        -<lore line 1>
+        -<lore line 2>
+        (continue for each line of lore)
+    enchantments:
+        <enchantment enum name>: <level>
+
+
+ */
---
+++ src/main/java/net/thejrdev/MissingConfigArgument.java
@@ -0,0 +1,8 @@
+package net.thejrdev;
+
+public class MissingConfigArgument extends Exception{
+
+    public MissingConfigArgument(String error){
+        super(error);
+    }
+}
